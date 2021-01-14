def06239868827abe799c48239e03bd33483a3d0
temorou <1115679568@qq.com>
2021-01-14 11:26:20 +0800

    兼容mocha

--- src/index.js
+++ src/index.js
@@ -19,6 +19,43 @@ function run(config={},callback){
 		callback.call(self,{page,window:self,route,router,store})
 	}).run()
 }
+function initMocha(){
+	['describe','it','before','after','beforeEach','afterEach'].forEach(key=>{
+		let buffer = global[key]
+		Object.defineProperty(global, key, {
+			get(){
+				return function (...args) {
+					args = args.map(arg=>{
+						if(typeof arg==='function'){
+							return async function (...a) {
+								let that = this
+								return await new Promise(function (resolve, reject) {
+									Fiber(function () {
+										let res
+										try {
+											res = arg.apply(that,a)
+											resolve(res)
+										}catch (e) {
+											reject(e)
+										}
+									}).run()
+								})
+							}
+						}
+						return arg
+					})
+					buffer.apply(this,args)
+				}
+			},
+			set(v) {
+				buffer = v
+			}
+		})
+	})
+}
+if(global.describe){
+	initMocha()
+}
 
 module.exports = {run,Fiber,...util}
 
